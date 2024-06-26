af0240bd82c5534c2779bb06683d1467b6203b7e
funnsam <funnsam8@gmail.com>
2024-06-26 18:00:55 +0800

    a

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,39 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(blog_os::test_runner)]
-#![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
 
-use blog_os::println;
 use core::panic::PanicInfo;
+
+mod gdt;
+mod interrupts;
+mod serial;
+mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
-    blog_os::init();
-
-    #[cfg(test)]
-    test_main();
+    init();
 
     println!("It did not crash!");
-    blog_os::hlt_loop();
+    hlt_loop();
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    blog_os::hlt_loop();
+    hlt_loop();
 }
 
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    blog_os::test_panic_handler(info)
+fn init() {
+    gdt::init();
+    interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() };
+    x86_64::instructions::interrupts::enable();
 }
 
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
 }
